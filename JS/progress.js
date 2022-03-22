81930fcb7ed74c1870bc3c8f50b4835aa4c59e9e
aust1inn <austinotieno364@gmail.com>
2022-03-22 10:20:26 +0300

    picks values for repairs done and puts them into an array

---
+++ JS/progress.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+$(document).ready(function () {
+    $("#progress").click(function () {
+
+        let carPerson = $("#car-owner").val();
+        let plate = $("#plate").val();
+
+        let engine = $("#engine").val();
+        let fuelSystem = $("#fuel").val();
+        let exhaustSystem = $("#exhaust").val();
+        let coolingSystem = $("#cooling").val();
+        let lubricationSystem = $("#lubrication").val();
+        let electricalSystem = $("#electriacal").val();
+        let transmission = $("#transmission").val();
+        let chassis = $("#chassis").val();
+        let completedParts = [];
+
+        $('input[name="repair"]:checked').each(function() {
+            completedParts.push(this.value);
+            });
+
+        let isComplete = $("#isComplete").val();
+        
+
+        alert(completedParts)
+    });
+});
