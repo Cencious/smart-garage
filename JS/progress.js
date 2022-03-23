625b607168f889e48eb1f645a31d4a8ab55fcf12
aust1inn <austinotieno364@gmail.com>
2022-03-23 08:27:11 +0300

    feat:sets the cost for various repairs

--- JS/progress.js
+++ JS/progress.js
@@ -28,15 +28,60 @@ $(document).ready(function () {
 
     $("#completion").click(function() {
 
+        let carPerson = $("#car-owner").val();
+        let plate = $("#plate").val();
+
+        let engine = $("#engine").val();
+        let fuelSystem = $("#fuel").val();
+        let exhaustSystem = $("#exhaust").val();
+        let coolingSystem = $("#cooling").val();
+        let lubricationSystem = $("#lubrication").val();
+        let electricalSystem = $("#electrical").val();
+        let transmission = $("#transmission").val();
+        let chassis = $("#chassis").val();
+        let completedParts = [];
+
+        $('input[name="repair"]:checked').each(function() {
+            completedParts.push(this.value);
+            });
         let isComplete = $("#isComplete").val();
 
-        if (isComplete == "yes") {
-           alert("user notified")
+       let repairPrice = 0
+       completedParts.forEach(completedPart => {
+           if (completedPart == "engine") {
+               repairPrice +=50000
+           }
+           else if (completedPart=="fuel-system") {
+                repairPrice +=18000
+           }
+           else if (completedPart=="exhaust-system") {
+               repairPrice +=22000
+           }
+           else if (completedPart == "cooling-system") {
+               repairPrice +=27000
+           }
+           else if (completedPart == "lubrication-system") {
+               repairPrice +=12000
+           }
+           else if (completedPart == "electrical-system") {
+               repairPrice +=55000
+           }
+           else if (completedPart == "transmission") {
+               repairPrice +=35000
+           }
+           else if (completedPart == "chassis") {
+               repairPrice +=25000
+           }
+       });
+
+        if (isComplete == "no") {
+           alert("repair incomplete")
         }
         else {
-            alert("repair incomplete")
+            $(".forms").hide();
+            $(".results").text(`${carPerson} successfully notified that the repair is complete`)
+            $(".price").text("Their total cost is Ksh" + repairPrice)
         }        
     });
-    
     
 });
