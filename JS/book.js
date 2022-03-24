83f254aee62ab541b6572e19feffc35df27fa936
catekui <catherine.wanjiku@student.moringaschool.com>
2022-03-24 08:51:32 +0300

    form validation

---
+++ JS/book.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $("#button").click(function () {
+        var name = document.getElementById("name").value
+        var email = document.getElementById("email").value
+        var myDate = document.getElementById("date").value
+        var time = document.getElementById("time").value
+
+        if (name == "" || email == "" || myDate == "" || time == "") {
+            alert("Please fill in the details")
+        } else {
+            alert("Hi " + name + "Thank you for booking with us. You have book on " + myDate + " at" + time)
+        }
+
+    })
+
+
+})
+
+
+
+
+
+
+
