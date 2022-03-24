0df71c00dc290c465557b27984d88e7619f1d4e3
ZippMukami <zimukami@gmail.com>
2022-03-24 12:20:58 +0300

    adds nav bar.

--- productJS/product.js
+++ productJS/product.js
@@ -123,3 +123,44 @@ function setItems(product) {
 }
 
 onLoadCartNumbers()
+$(document).ready();{
+
+const prod = $(".product-page");
+
+
+prod.hide();
+
+$(".cart").click(
+    function (){
+        prod.show();
+    }
+)
+}
+
+
+// $(document).ready();{
+
+//     const card_wrap = $(".services").find('p');
+//     const card1 = $(".card1").find("p");
+//     const card2 = $(".card2").find("p");
+//     const card3 = $(".card3").find("p");
+//     const card4 = $(".card4").find("p");
+//     const card5 = $(".card5").find("p");
+//     const card6 = $(".card6").find("p");
+
+
+
+
+//     card_wrap.hide();
+
+//     $(".card1").click(
+//         function () {
+//             if (card1.is(":visible")) {
+//                 card1.hide();
+//             } else {
+//                 card1.show();
+//             }
+//         }
+
+//     )
+//     }
