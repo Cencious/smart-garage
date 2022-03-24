a4f065265dd0ebdd932a760b7759114dd243fa8e
Cencious <user.email>
2022-03-24 16:29:49 +0300

    add jquery

--- js/about.js
+++ js/about.js
@@ -1,8 +1,14 @@
-$(document).ready(function(){
-    $(".us").click(function (){
-       $(this).find(".us").toggle();
-       $(this).find(".hidden-p").toggle(); 
-    })
-
-    
-})
+$(document).ready(function () {
+  $(".vision").click(function () {
+    $(".vision-a").toggle();
+    $(".vision-p").toggle();
+  });
+  $(".mission").click(function () {
+    $(".mission-a").toggle();
+    $(".mission-p").toggle();
+  });
+  $(".value").click(function () {
+    $(".value-a").toggle();
+    $(".value-p").toggle();
+  });
+});
