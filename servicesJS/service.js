8fb62f43c87aca0ffc6c3716c78ca9b755d9bf52
ZippMukami <zimukami@gmail.com>
2022-03-24 18:44:03 +0300

    adds show and hide functions.

--- servicesJS/service.js
+++ servicesJS/service.js
@@ -1,27 +1,27 @@
-// $(document).ready();{
+$(document).ready();{
 
-//     const card_wrap = $(".services").find('p');
-//     const card1 = $(".card1").find("p");
-//     const card2 = $(".card2").find("p");
-//     const card3 = $(".card3").find("p");
-//     const card4 = $(".card4").find("p");
-//     const card5 = $(".card5").find("p");
-//     const card6 = $(".card6").find("p");
-
-
+    const card_wrap = $(".services").find('p');
+    const card1 = $(".card1").find("p");
+    const card2 = $(".card2").find("p");
+    const card3 = $(".card3").find("p");
+    const card4 = $(".card4").find("p");
+    const card5 = $(".card5").find("p");
+    const card6 = $(".card6").find("p");
 
 
-//     card_wrap.hide();
 
-//     $(".card1").click(
-//         function () {
-//             if (card1.is(":visible")) {
-//                 card1.hide();
-//             } else {
-//                 card1.show();
-//             }
-//         }
 
-//     )
+    card_wrap.hide();
 
-// };
+    $(".card1").click(
+        function () {
+            if (card1.is(":visible")) {
+                card1.hide();
+            } else {
+                card1.show();
+            }
+        }
+
+    )
+
+};
