3b6120263d6f1b18f0a2ab097fe55ab03b98a8e8
ZippMukami <zimukami@gmail.com>
2022-03-24 09:30:36 +0300

    adds product tags.

--- productJS/product.js
+++ productJS/product.js
@@ -98,3 +98,28 @@ function cartNumber(product) {
     };
     setItems(product);
 };
+
+function setItems(product) {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    if (cartItems != null) {
+        if (cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+
+        cartItems = {
+            [product.tag]: product
+        }
+
+    }
+
+    localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+}
+
+onLoadCartNumbers()
