40c49cb404232dde24cd65e1f482634cacf6fc10
ZippMukami <zimukami@gmail.com>
2022-03-24 18:45:19 +0300

    adds styles to cards.

--- servicesJS/service.js
+++ servicesJS/service.js
@@ -24,4 +24,62 @@ $(document).ready();{
 
     )
 
+
+    $(".card2").click(
+        function () {
+            if (card2.is(":visible")) {
+                card2.hide();
+            } else {
+                card2.show();
+            }
+        }
+    )
+
+
+
+    $(".card3").click(
+        function () {
+            if (card3.is(":visible")) {
+                card3.hide();
+            } else {
+                card3.show();
+            }
+        }
+
+    )
+
+    $(".card4").click(
+        function () {
+            if (card4.is(":visible")) {
+                card4.hide();
+            } else {
+                card4.show();
+            }
+        }
+
+    )
+
+    $(".card5").click(
+        function () {
+            if (card5.is(":visible")) {
+                card5.hide();
+            } else {
+                card5.show();
+            }
+        }
+
+    )
+
+    $(".card6").click(
+        function () {
+            if (card6.is(":visible")) {
+                card6.hide();
+            } else {
+                card6.show();
+            }
+        }
+
+    )
+
+
 };
