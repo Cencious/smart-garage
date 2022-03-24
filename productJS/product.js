fcbc108a875e3587322fcba7a82798028eeaeddf
ZippMukami <zimukami@gmail.com>
2022-03-24 15:23:02 +0300

    adds total cost to products.

--- productJS/product.js
+++ productJS/product.js
@@ -1,69 +1,68 @@
-
 let carts = document.querySelectorAll(".add-cart");
 
 let products = [{
-    name: "suspension",
-    tag: "car suspension",
-    image: "suspension.jpg",
-    price: 10000,
-    inCart: 0
-},
+        name: "suspension",
+        tag: "car suspension",
+        image: "suspension.jpg",
+        price: 10000,
+        inCart: 0
+    },
 
-{
-    name: "car wash",
-    tag: "car-wash",
-    image: "car-wash.jpg",
-    price: 10000,
-    inCart: 0
-},
+    {
+        name: "car wash",
+        tag: "car-wash",
+        image: "car-wash.jpg",
+        price: 10000,
+        inCart: 0
+    },
 
-{
-    name: "engine",
-    tag: "car engine",
-    image: "engine.jpeg",
-    price: 100000,
-    inCart: 0
-},
+    {
+        name: "engine",
+        tag: "car engine",
+        image: "engine.jpeg",
+        price: 100000,
+        inCart: 0
+    },
 
-{
-    name: "lights",
-    tag: "car lights",
-    image: "light2.jpeg",
-    price: 10000,
-    inCart: 0
-},
+    {
+        name: "lights",
+        tag: "car lights",
+        image: "light2.jpeg",
+        price: 10000,
+        inCart: 0
+    },
 
-{
-    name: "seat",
-    tag: "car seat",
-    image: "car-seat.jpeg",
-    price: 10000,
-    inCart: 0
-},
+    {
+        name: "seat",
+        tag: "car seat",
+        image: "car-seat.jpeg",
+        price: 10000,
+        inCart: 0
+    },
 
-{
-    name: "door",
-    tag: "red door",
-    image: "red-door.jpeg",
-    price: 10000,
-    inCart: 0
-},
+    {
+        name: "door",
+        tag: "red door",
+        image: "red-door.jpeg",
+        price: 10000,
+        inCart: 0
+    },
 
-{
-    name: "brakes",
-    tag: "pads brakes",
-    image: "break-pads.jpeg",
-    price: 10000,
-    inCart: 0
-},
+    {
+        name: "brakes",
+        tag: "pads brakes",
+        image: "break-pads.jpeg",
+        price: 10000,
+        inCart: 0
+    },
 
-{
-    name: "bonnet",
-    tag: "car bonnet",
-    image: "bonnet.jpeg",
-    price: 10000,
-    inCart: 0
-},
+    {
+        name: "bonnet",
+        tag: "car bonnet",
+        image: "bonnet.jpeg",
+        price: 10000,
+        inCart: 0
+    },
 
 
 ]
@@ -123,44 +122,35 @@ function setItems(product) {
 }
 
 onLoadCartNumbers()
-$(document).ready();{
 
-const prod = $(".product-page");
-
-
-prod.hide();
-
-$(".cart").click(
-    function (){
-        prod.show();
+function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost');
+    if (cartCost != null) {
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + product.price);
+    } else {
+        localStorage.setItem("totalCost", product.price);
     }
-)
+
+    console.log("The product price is", product.price);
+};
+
+
+
+
+
+
+
+$(document).ready(); {
+
+    const prod = $(".product-page");
+
+
+    prod.hide();
+
+    $(".cart").click(
+        function () {
+            prod.show();
+        }
+    )
 }
-
-
-// $(document).ready();{
-
-//     const card_wrap = $(".services").find('p');
-//     const card1 = $(".card1").find("p");
-//     const card2 = $(".card2").find("p");
-//     const card3 = $(".card3").find("p");
-//     const card4 = $(".card4").find("p");
-//     const card5 = $(".card5").find("p");
-//     const card6 = $(".card6").find("p");
-
-
-
-
-//     card_wrap.hide();
-
-//     $(".card1").click(
-//         function () {
-//             if (card1.is(":visible")) {
-//                 card1.hide();
-//             } else {
-//                 card1.show();
-//             }
-//         }
-
-//     )
-//     }
